b5f1e2f8bbb6fd6e1c0d1a3e201f2df80de6b5a9
Ramesh Indrajith <ramesh.kumar@snaile.com>
2023-01-26 23:14:29 +0530

    Remove ECK

--- main.tf
+++ main.tf
@@ -17,18 +17,21 @@ module "redis" {
   ]
 }
 
-#Elastic Operator
-module "eck_operator" {
-  source           = "./modules/eck-operator"
-  module_namespace = local.elastic_namespace
-  depends_on = [
-    module.eks
-  ]
-}
+# #Elastic Operator
+# module "eck_operator" {
+#   source           = "./modules/eck-operator"
+#   module_namespace = local.elastic_namespace
+#   depends_on = [
+#     module.eks
+#   ]
+# }
 
-#Elastic Cluster
-module "elastic_cluster" {
-  source              = "./modules/kubernetes-resource-from-yaml"
-  override_namespace  = local.elastic_namespace
-  resource_definition = local.elastic_cluster_deployment_yaml
-}
+# #Elastic Cluster
+# module "elastic_cluster" {
+#   source              = "./modules/kubernetes-resource-from-yaml"
+#   override_namespace  = local.elastic_namespace
+#   resource_definition = local.elastic_cluster_deployment_yaml
+#   depends_on = [
+#     module.eck_operator
+#   ]
+# }
