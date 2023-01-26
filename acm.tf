c4d215935fe0faac2b2ffb8ec238f5fc3d3f5d5c
Ramesh Indrajith <ramesh.kumar@snaile.com>
2023-01-27 00:15:20 +0530

    Fix ACM

--- acm.tf
+++ acm.tf
@@ -10,29 +10,4 @@ module "acm" {
     "*.${var.domain_name}",
     var.domain_name
   ]
-
-}
-
-resource "aws_route53_record" "dvo" {
-  for_each = {
-    for dvo in module.acm.acm_certificate_domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.zone.zone_id
-  depends_on = [
-    module.acm
-  ]
-}
-
-resource "aws_acm_certificate_validation" "this" {
-  certificate_arn         = module.acm.acm_certificate_arn
-  validation_record_fqdns = [for record in aws_route53_record.dvo : record.fqdn]
 }
