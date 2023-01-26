c82525953b85d0f4ea2d496468ade5bf04e7adcf
Ramesh Indrajith <ramesh.kumar@snaile.com>
2023-01-27 01:32:17 +0530

    Fix Ingress

--- modules/nginx-ingress/ingress.tf
+++ modules/nginx-ingress/ingress.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "aws_ingress" {
-  name             = var.module_ingress_class
+  name             = "ingress-nginx"
   chart            = "ingress-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx"
   version          = var.module_chart_version
