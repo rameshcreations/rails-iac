56bf7d8519a88675763f8661dba13f62ce000538
Ramesh Indrajith <ramesh.kumar@snaile.com>
2023-01-26 14:02:04 +0530

    Remove EKS Cluster

--- provider.tf
+++ provider.tf
@@ -4,8 +4,3 @@ provider "aws" {
     tags = var.tags
   }
 }
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
